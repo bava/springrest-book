33226119caf6856867e5ddfadd74a412fda0dd5a
Balaji <Balaji.Varanasi@gmail.com>
2015-05-23 19:24:04 -0600

    Removed the import

--- Chapter5/final/quick-poll-ch5-final/src/main/java/com/apress/QuickPollApplication.java
+++ Chapter5/final/quick-poll-ch5-final/src/main/java/com/apress/QuickPollApplication.java
@@ -2,7 +2,6 @@ package com.apress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class QuickPollApplication {
