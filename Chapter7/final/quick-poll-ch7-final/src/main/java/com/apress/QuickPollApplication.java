54fecfeb94e6f0fbb091278ac41dbc16b218b7ca
Balaji <Balaji.Varanasi@gmail.com>
2015-05-24 16:28:28 -0600

    Removed the commented code

--- Chapter7/final/quick-poll-ch7-final/src/main/java/com/apress/QuickPollApplication.java
+++ Chapter7/final/quick-poll-ch7-final/src/main/java/com/apress/QuickPollApplication.java
@@ -11,13 +11,5 @@ public class QuickPollApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuickPollApplication.class, args);
-    }
-    
-//    @Bean
-//	public PageableHandlerMethodArgumentResolver resolver() {
-//    	PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
-//    	phmar.setFallbackPageable(new PageRequest(0, 5));
-//		return phmar;
-//	}
-    
+    }    
 }
