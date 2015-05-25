1e028f4e4f650c0a264875ce6496bbc924f48475
Sudha Belida <Sudha.Belida@gmail.com>
2015-05-25 15:13:10 -0600

    Commiting with start and final folder structure

---
+++ Chapter3/starter/hello-rest/src/main/java/com/apress/hellorest/HelloWorldRestApplication.java
@@ -0,0 +1,13 @@
+package com.apress.hellorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldRestApplication.class, args);
+    }
+	
+}
